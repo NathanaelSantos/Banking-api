b13948284a8bad17ff65cfc0e022f2035e9929fb
Nathan Santos <nathanaelsantos15@gmail.com>
2024-06-29 03:05:22 -0300

    feat(service): enhance BankService with exception handling and logging

--- src/main/java/org/acme/service/BankService.java
+++ src/main/java/org/acme/service/BankService.java
@@ -1,20 +1,35 @@
 package org.acme.service;
 
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.acme.Exception.bank.NoBanksFoundException;
+import org.acme.Exception.service.BankServiceException;
 import org.acme.model.BankModel;
 import org.acme.repository.BankRepository;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class BankService {
+
+    private static final Logger log = Logger.getLogger(BankService.class.getName());
 
     @Inject
     BankRepository bankRepository;
 
     public List<BankModel> getAllBanks() {
-        return bankRepository.listAll();
+        try {
+            List<BankModel>  banks = bankRepository.listAll();
+            if (banks.isEmpty()) {
+                log.warning("No banks found in the database");
+                throw new NoBanksFoundException();
+            }
+            return banks;
+        }catch (Exception e){
+            log.log(Level.SEVERE, "Error getting all banks", e);
+            throw new BankServiceException("An error occurred while getting all banks", e);
+        }
     }
 }
